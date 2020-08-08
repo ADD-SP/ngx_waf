d11be0b2781a29496c07eb397798d46afa89b828
ADD-SP <me@addesp.com>
2020-08-09 03:02:30 +0800

    :triangular_flag_on_post: Mount to NGX_HTTP_POST_READ_PHASE

--- ngx_http_waf_module.c
+++ ngx_http_waf_module.c
@@ -197,7 +197,7 @@ static ngx_int_t ngx_http_waf_init_after_load_config(ngx_conf_t* cf) {
     ngx_http_core_main_conf_t* cmcf;
 
     cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
-    h = ngx_array_push(&cmcf->phases[NGX_HTTP_CONTENT_PHASE].handlers);
+    h = ngx_array_push(&cmcf->phases[NGX_HTTP_POST_READ_PHASE].handlers);
     if (h == NULL) {
         return NGX_ERROR;
     }
