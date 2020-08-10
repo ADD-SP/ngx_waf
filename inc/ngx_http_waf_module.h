579ea4f21881d7bed7f5d9089d99e1c0e244847a
ADD-SP <add_sp@outlook.com>
2020-08-10 23:20:51 +0800

    :bulb: Add or update comments in source code.

--- inc/ngx_http_waf_module.h
+++ inc/ngx_http_waf_module.h
@@ -48,34 +48,35 @@ typedef struct {
 } hash_table_item_int_ulong_t;
 
 typedef struct {
-    ngx_log_t* ngx_log;                         /* 记录内存池在进行操作时的错误日志 */
-    ngx_pool_t* ngx_pool;                       /* 模块所使用的内存池 */
-    ngx_uint_t alloc_times;                     /* 当前已经从内存池中申请过多少次内存 */
-    ngx_int_t ngx_waf;                          /* 是否启用本模块 */
-    ngx_str_t ngx_waf_rule_path;                /* 配置文件所在目录 */
-    ngx_int_t ngx_waf_cc_deny;                  /* 是否启用 CC 防御 */
-    ngx_int_t ngx_waf_cc_deny_limit;            /* CC 防御的限制频率 */
-    ngx_int_t ngx_waf_cc_deny_duration;         /* CC 防御的拉黑时长 */
-    ngx_array_t* block_ipv4;
-    ngx_array_t* block_url;
-    ngx_array_t* block_args;
-    ngx_array_t* block_ua;
-    ngx_array_t* block_referer;
-    ngx_array_t* white_ipv4;
-    ngx_array_t* white_url;
-    ngx_array_t* white_referer;
-    hash_table_item_int_ulong_t* ipv4_times;
+    ngx_log_t                      *ngx_log;                    /* 记录内存池在进行操作时的错误日志 */
+    ngx_pool_t                     *ngx_pool;                   /* 模块所使用的内存池 */
+    ngx_uint_t                      alloc_times;                /* 当前已经从内存池中申请过多少次内存 */
+    ngx_int_t                       ngx_waf;                    /* 是否启用本模块 */
+    ngx_str_t                       ngx_waf_rule_path;          /* 配置文件所在目录 */
+    ngx_int_t                       ngx_waf_cc_deny;            /* 是否启用 CC 防御 */
+    ngx_int_t                       ngx_waf_cc_deny_limit;      /* CC 防御的限制频率 */
+    ngx_int_t                       ngx_waf_cc_deny_duration;   /* CC 防御的拉黑时长 */
+    ngx_array_t                    *block_ipv4;                 /* IPV4 黑名单 */
+    ngx_array_t                    *block_url;                  /* URL 黑名单 */
+    ngx_array_t                    *block_args;                 /* args 黑名单 */
+    ngx_array_t                    *block_ua;                   /* user-agent 黑名单 */
+    ngx_array_t                    *block_referer;              /* Referer 黑名单 */
+    ngx_array_t                    *white_ipv4;                 /* IPV4 白名单 */
+    ngx_array_t                    *white_url;                  /* URL 白名单 */
+    ngx_array_t                    *white_referer;              /* Referer 白名单 */
+    hash_table_item_int_ulong_t    *ipv4_times;                 /* IPV4 访问频率统计表 */
 
-    ngx_pool_t* ngx_pool_old;
-    hash_table_item_int_ulong_t* ipv4_times_old;
-    hash_table_item_int_ulong_t* ipv4_times_old_cur;
-    ngx_int_t free_hash_table_step;
+    ngx_pool_t                     *ngx_pool_old;               /* 执行函数 free_hash_table 时用于备份旧的内存池 */
+    hash_table_item_int_ulong_t    *ipv4_times_old;             /* 执行函数 free_hash_table 时用于备份旧的 IPV4 访问频率统计表 */
+    hash_table_item_int_ulong_t    *ipv4_times_old_cur;         /* 执行函数 free_hash_table 时用于记录当前处理到旧的 IPV4 访问频率统计表的哪一项 */
+    ngx_int_t                       free_hash_table_step;       /* 记录 free_hash_table 执行到哪一阶段 */
 }ngx_http_waf_srv_conf_t;
 
 typedef struct {
-    size_t prefix;
-    size_t suffix;
+    size_t prefix;  /* 相当于 192.168.1.0/24 中的 192.168.1.0 的整数形式 */
+    size_t suffix;  /* 相当于 192.168.1.0/24 中的 24 的整数形式 */
 }ipv4_t;
+
 
 static char* ngx_http_waf_conf(ngx_conf_t* cf, ngx_command_t* cmd, void* conf);
 
