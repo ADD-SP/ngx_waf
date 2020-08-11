ad1dbd8707a06a706cafe80e69c0346cb644dcfc
ADD-SP <me@addesp.com>
2020-08-11 09:56:04 +0800

    :triangular_flag_on_post: Mount to NGX_HTTP_SERVER_REWRITE_PHASE

--- src/ngx_http_waf_module.c
+++ src/ngx_http_waf_module.c
@@ -179,7 +179,7 @@ static ngx_int_t ngx_http_waf_init_after_load_config(ngx_conf_t* cf) {
     ngx_http_core_main_conf_t* cmcf;
 
     cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
-    h = ngx_array_push(&cmcf->phases[NGX_HTTP_PREACCESS_PHASE].handlers);
+    h = ngx_array_push(&cmcf->phases[NGX_HTTP_SERVER_REWRITE_PHASE].handlers);
     if (h == NULL) {
         return NGX_ERROR;
     }
