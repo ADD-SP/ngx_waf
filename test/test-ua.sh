45c900338a9d2476ba99c4cb615036f2ec8475c1
ADD-SP <me@addesp.com>
2021-03-02 10:55:12 +0800

    Update test-ua.sh

--- test/test-ua.sh
+++ test/test-ua.sh
@@ -7,7 +7,7 @@ for user_agent in $user_agents ; do
     curl_opt="-A '${user_agent}'"
     echo "Testing User-Agent: ${user_agent}"
     . test/get-http-status.sh
-    if [ $http_status -ne 403 ] ; then
+    if [ "$http_status" -ne 403 ] ; then
         exit 1
     fi
 done
