a943cf90299a1de183b3ddc775ba8735dceeb5d0
ADD-SP <me@addesp.com>
2021-03-02 11:28:02 +0800

    Update init-conf.sh

--- test/init-conf.sh
+++ test/init-conf.sh
@@ -9,10 +9,10 @@ if [ -e "${ngx_root}/conf/nginx.conf" ] ; then
     rm -rf "${ngx_root}/conf/nginx.conf"
 fi
 
-if [ $opt = '--add-module' ] ; then
+if [ “$opt” = '--add-module' ] ; then
     cp ${new_static_module_conf} "${ngx_root}/conf/nginx.conf"
 else
     cp ${new_dynamic_module_conf} "${ngx_root}/conf/nginx.conf"
 fi
 
-cp -r ${rules_dir} "${ngx_root}/conf/rules"
+cp -r “${rules_dir}” "${ngx_root}/conf/rules"
