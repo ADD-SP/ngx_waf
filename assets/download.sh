719ff1147d7875d0fdb75f947f61ac404d3e89e2
ADD-SP <me@addesp.com>
2021-07-21 21:39:40 +0800

    :bento: Update file `assets/download.sh`

--- assets/download.sh
+++ assets/download.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-if [ "$1" != stable -a "$1" != mainline ] ; then
-    echo "$1: invalid argument"
-    exit 22
-fi
-
-if [ "$2" != stable -a "$2" != beta ] ; then
-    echo "$1: invalid argument"
-    exit 22
-fi
-
 echo -n "checking for command ... "
 
 which find > /dev/null
@@ -39,7 +29,7 @@ echo -n "checking for libc implementation ... "
 
 imp=''
 
-tmp=`find -L /lib* -regex \.*libc\.so.*` 2> /dev/null
+tmp=`find -L /lib* -regex \.*libc\.so.* 2> /dev/null`
 
 echo "$tmp" | grep gnu > /dev/null
 if [ $? -eq 0 ] ; then
@@ -58,23 +48,21 @@ else
     fi
 fi
 
-echo -n "pulling remote image addsp/ngx_waf-prebuild:ngx-$1-module-$2-$tmp ... "
-tmp=`docker pull "addsp/ngx_waf-prebuild:ngx-$1-module-$2-$tmp"` 2> /dev/null
-if [ $? -ne 0 ] ; then
-    echo 'no'
-    exit 121
-else
-    echo 'yes'
+echo "Pulling remote image addsp/ngx_waf-prebuild:ngx-$1-module-$2-$imp"
+docker pull "addsp/ngx_waf-prebuild:ngx-$1-module-$2-$imp"
+if [ $? -eq 0 ] ; then
+    tmpdir=`head -10 /dev/urandom | md5sum - | cut -c 1-32`
+    while [ -e $(pwd)/tmpdir ]
+    do
+        tmpdir=`head -10 /dev/urandom | md5sum - | cut -c 1-32`
+    done
+
+    tmp=`docker run --rm -d -v "$(pwd)/$tmpdir":/out "addsp/ngx_waf-prebuild:ngx-$1-module-$2-$imp" cp /modules/ngx_http_waf_module.so /out` 2> /dev/null
+
+    cp "$(pwd)/$tmpdir/ngx_http_waf_module.so" ./
+    rm -rf "$(pwd)/$tmpdir"
+
+    echo "Download complete!"
 fi
 
 
-tmpdir=`head -10 /dev/urandom | md5sum - | cut -c 1-32`
-while [ -e $(pwd)/tmpdir ]
-do
-    tmpdir=`head -10 /dev/urandom | md5sum - | cut -c 1-32`
-done
-
-tmp=`docker run --rm -d -v "$(pwd)/$tmpdir":/out "addsp/ngx_waf-prebuild:ngx-$1-module-$2-$tmp" cp /modules/ngx_http_waf_module.so /out` 2> /dev/null
-
-cp "$(pwd)/$tmpdir/ngx_http_waf_module.so" ./
-rm -rf "$(pwd)/$tmpdir"
