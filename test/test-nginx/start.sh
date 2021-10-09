b36cdb6ea7d3ec5c049880628804b7ae77e40018
ADD-SP <me@addesp.com>
2021-10-09 21:07:14 +0800

    :white_check_mark: Update `test/test-nginx/start.sh`.

--- test/test-nginx/start.sh
+++ test/test-nginx/start.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-export PATH=/usr/local/openresty/nginx/sbin:$PATH
+export PATH=/usr/local/openresty/nginx/sbin:/usr/local/nginx/sbin:$PATH
 
 if [ -e "/usr/local/nginx/modules/ngx_http_waf_module.so" ] ; then
     export TEST_NGINX_LOAD_MODULES=/usr/local/nginx/modules/ngx_http_waf_module.so
+fi
+
+if [ -n "$MODULE_PATH" ] ; then
+    export TEST_NGINX_LOAD_MODULES="$MODULE_PATH"
 fi
 
 # export TEST_NGINX_LOG_LEVEL=emerg
