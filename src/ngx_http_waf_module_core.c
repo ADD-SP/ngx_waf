44b816e77e094cd6ca0763969cc9ae7c48f2a089
ADD-SP <add_sp@outlook.com>
2021-11-15 11:22:14 +0800

    :bug: Syntax error of directive `waf_action`.

--- src/ngx_http_waf_module_core.c
+++ src/ngx_http_waf_module_core.c
@@ -88,7 +88,7 @@ static ngx_command_t ngx_http_waf_commands[] = {
    },
    {
         ngx_string("waf_action"),
-        NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_CONF_TAKE123,
+        NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1234 | NGX_CONF_TAKE5 | NGX_CONF_TAKE6 | NGX_CONF_TAKE7,
         ngx_http_waf_action_conf,
         NGX_HTTP_LOC_CONF_OFFSET,
         0,
